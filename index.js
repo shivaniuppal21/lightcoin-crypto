bd66938aae93da06d69c11a2e6ceb387931dce2b
shivani <shivaniuppal21@gmail.com>
2021-01-15 04:59:08 +0000

    completed oop training

--- index.js
+++ index.js
@@ -1,29 +1,95 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.transactions = [];
   }
 
+  get balance() {
+    // Calculate the balance using the transaction objects.
+    if (this.transactions.length === 0){
+      return 0;
+    }
+    let balance = 0;
+    for (let trans of this.transactions){
+       balance += trans.value
+    }
+    return balance
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+  
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    // Add the transaction to the account
+    if(this.isAllowed()){
+      this.account.addTransaction(this);
+    }else{
+      console.log("invalid transaction")
+    }
+  }
+}
+
+class Deposit extends Transaction {
+  isAllowed(){
+    return true
+    }
+  get value(){
+    return this.amount
+  }
+}
+
+class Withdrawal extends Transaction {
+  isAllowed(){
+    if(this.account.balance < this.amount){
+      return false;
+    }else{
+      return true;
+    }
+  }
+  get value(){
+    return (-this.amount)
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+t1 = new Withdrawal(50.25,myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+//console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99,myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+//console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
+
+t3 = new Deposit(120.00,myAccount);
+t3.commit();
+//console.log('Transaction 3:', t3);
+console.log('Balance:', myAccount.balance);
+
+t4 = new Withdrawal(50.25,myAccount);
+t4.commit();
+console.log('Balance:', myAccount.balance);
+
